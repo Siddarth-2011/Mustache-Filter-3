c9ad4690fd94ab4b5ec557144f6e851f37e68bcb
Siddarth-2011 <81513284+Siddarth-2011@users.noreply.github.com>
2022-01-15 10:55:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+noseX=0;
+noseY=0;
+
+function preload() {
+ mustache = loadImage('https://i.postimg.cc/TPBnxgqY/png-clipart-moustache-moustache-removebg-preview.png');
+}
+
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(300, 300);
+  video.hide();
+
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+  console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    console.log(results);
+    noseX = results[0].pose.nose.x -43;
+    noseY = results[0].pose.nose.y -43;
+    console.log("nose x = " + noseX);
+    console.log("nose y = " + noseY);    
+  }
+}
+
+function draw() {
+  image(video, 0, 0, 300, 300);
+  image(mustache, noseX, noseY, 85, 85);
+}
+
+function take_snapshot(){    
+  save('myFilterImage.png');
+}
